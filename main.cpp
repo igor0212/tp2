36d07f4e40f983494447fb786fb76e6e11f3e0ce
igor.silveira <igor.silveira@dtidigital.com.br>
2019-09-23 13:06:52 -0300

    Calcular tempo

--- main.cpp
+++ main.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
-//#include <fstream>
-//#include <string>
-//#include <vector>
 #include <stdio.h>
-//#include <conio.h>
 #include "include/tp1.hpp"
+#include <time.h>
 
 using namespace std;
 
 int main(int argc, char* argv[])
-{		
+{	
+	int resultado;	
+	clock_t tInicio, tFim, tDecorrido;	
+
+	tInicio = clock();
+
 	if (argc != 2) {
 		cout <<  "Argumento invalido";		
-		return 0;
+		resultado = 0;
 	}
 	else {					
 		string equipeFile = argv[argc-1];			
 		StartTP1(equipeFile);
-		return 1;
+		resultado = 1;
 	}		
+
+	tFim = clock();
+
+	//tDecorrido = ((tFim - tInicio) / (CLOCKS_PER_SEC / 1000));
+	
+	//cout << "\nTempo decorrido: " << tDecorrido << " milisegundos \n";
+
+	return resultado;
 }
